5223d2b213cfa97cdb1afec8801ead486908e617
Ashurov Safarmurod <92220640+Safarmurod7@users.noreply.github.com>
2023-02-21 01:05:16 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PharmacyEdition_2._0
+{
+#pragma warning disable 
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
